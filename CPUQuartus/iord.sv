1102ebb6945a4b53415577eff151244d7e7e508e
Francisco Marcos Lira do Nascimento <fmln@WINDOWS.CIN.UFPE.BR>
2019-10-09 17:47:19 -0300

    iord ajustado

--- CPUQuartus/iord.sv
+++ CPUQuartus/iord.sv
@@ -1,7 +1,7 @@
 module iord(
 	input logic [31:0] pcOut,
-	input logic [31:0] aluResult,
 	input logic [31:0] S,
+	input logic [31:0] Saida,
 	output logic [31:0] iordOut,
 	input logic [2:0]iordmux
 );
@@ -14,8 +14,8 @@ parameter  F = 32'd255;
 always
 	case(iordmux)
 		3'b000: iordOut = pcOut;
-		3'b001: iordOut = aluResult;
-		3'b010: iordOut = S;
+		3'b001: iordOut = S;
+		3'b010: iordOut = Saida;
 		3'b011: iordOut = D;
 		3'b100: iordOut = E;
 		3'b101: iordOut = F;
